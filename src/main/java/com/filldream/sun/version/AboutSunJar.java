8918742a906191f8e02e4b066c61f5529c72102f
SunGitHub97 <zczhzymail@163.com>
2019-02-24 11:55:25 +0800

    版本更新

--- src/main/java/com/filldream/sun/version/AboutSunJar.java
+++ src/main/java/com/filldream/sun/version/AboutSunJar.java
@@ -3,11 +3,11 @@ package com.filldream.sun.version;
 public class AboutSunJar {
 	public static void about() {
 		System.out.println("软件名：SunJar");
-		System.out.println("软件版本：0.6.2.40");
+		System.out.println("软件版本：0.6.5.40");
 		System.out.println("作者：孙连磊");
 		System.out.println("版权：微信公众号iFillDream和孙连磊共有");
 		System.out.println("创建时间：2018-06-23");
-		System.out.println("修改时间：2019-02-23");
+		System.out.println("修改时间：2019-02-24");
 		System.out.println("该Jar包仅供参考，导致的任何商业问题概不负责，版权问题请私聊");
 	}
 	public static void main(String[] args) {
