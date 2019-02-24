105b1d3eff62af8309feae7845c46213eae99a7e
SunGitHub97 <zczhzymail@163.com>
2019-02-24 21:20:30 +0800

    修改方法名

--- src/main/java/com/filldream/sun/utils/EncryptUtil.java
+++ src/main/java/com/filldream/sun/utils/EncryptUtil.java
@@ -26,7 +26,7 @@ public class EncryptUtil {
 	 * @param text
 	 * @return
 	 */
-	public static String Base64Encode(String content) {
+	public static String base64Encode(String content) {
 		return Base64.encodeBase64String(content.getBytes());
 	}
 	
@@ -36,11 +36,11 @@ public class EncryptUtil {
 	 * @param key
 	 * @return
 	 */
-	public static String Base64Encode(String content, String key) {
+	public static String base64Encode(String content, String key) {
 		if (SunCommon.isNotBlank(key)) {
 			content = content + key;
 		}
-		return Base64Encode(content);
+		return base64Encode(content);
 	}
 	
 	
@@ -49,7 +49,7 @@ public class EncryptUtil {
 	 * @param text
 	 * @return
 	 */
-	public static String Base64Decode(String content) {
+	public static String base64Decode(String content) {
 		return new String(Base64.decodeBase64(content));
 	}
 	
@@ -60,8 +60,8 @@ public class EncryptUtil {
 	 * @param base64
 	 * @return
 	 */
-	public static boolean Base64Decode(String content, String key, String base64) {
-        String myBase64 = Base64Encode(content, key);
+	public static boolean base64Decode(String content, String key, String base64) {
+        String myBase64 = base64Encode(content, key);
         if (myBase64.equals(base64)) {
             return true;
         } else {
@@ -93,14 +93,14 @@ public class EncryptUtil {
 	 * @param second
 	 * @return
 	 */
-	public static String setToken(Integer second) {
+	public static String setSimpleToken(Integer second) {
 		Date overTime =DateUtil.getDateByAddCalendar(new Date(), Calendar.SECOND, second);
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyyMMddHHmmsss");
 		
 		String time = simpleDateFormat.format(overTime);
 		
 		String token = getUUID() + time;
-		return RandomUtil.randAlphaNumString(3).toUpperCase()+Base64Encode(token);
+		return RandomUtil.randAlphaNumString(3).toUpperCase()+base64Encode(token);
 	}
 	
 	/**
@@ -109,10 +109,10 @@ public class EncryptUtil {
 	 * @param projectKey
 	 * @return  0-正确 -1错误 -1002过时
 	 */
-	public static int checkToken(String token) {
+	public static int checkSimpleToken(String token) {
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddHHmmsss");
 		try {
-			String jiemi1 = Base64Decode(token.substring(3, token.length()));
+			String jiemi1 = base64Decode(token.substring(3, token.length()));
 			String time = jiemi1.substring(jiemi1.length() - 15,jiemi1.length());
 			
 			if(Long.valueOf(time) >= Long.valueOf(sdf.format(new Date()))) {
