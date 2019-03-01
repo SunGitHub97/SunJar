56fa1a3015ce1b5088a90be315c9a0a13d8ec97d
SunGitHub97 <zczhzymail@163.com>
2019-03-01 20:31:47 +0800

    优化

--- src/main/java/com/filldream/sun/utils/CheckUtil.java
+++ src/main/java/com/filldream/sun/utils/CheckUtil.java
@@ -8,11 +8,12 @@ import java.util.regex.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.filldream.sun.constants.FinalString;
+
 
 public class CheckUtil {
 	private static final Logger logger = LoggerFactory.getLogger(CheckUtil.class);
 	
-	public static final String NATION = "汉,壮,满,回,苗,维吾尔,土家,彝,蒙古,藏,布依,侗,瑶,朝鲜,白,哈尼,哈萨克,黎,傣,畲,傈僳,仡佬,东乡,高山,拉祜,水,佤,纳西,羌,土,仫佬,锡伯,柯尔克孜,达斡尔,景颇,毛南,撒拉,布朗,塔吉克,阿昌,普米,鄂温克,怒,京,基诺,德昂,保安,俄罗斯,裕固,乌孜别克,门巴,鄂伦春,独龙,塔塔尔,赫哲,珞巴";
 	
 	/**
 	 * 判断是否是56个民族
@@ -21,7 +22,7 @@ public class CheckUtil {
 	 * @return
 	 */
 	public static boolean checkNation(String nation,Boolean addClan) {
-		String[] nations = NATION.split(",");
+		String[] nations = FinalString.NATION.split(",");
 		if(addClan == false) {
 			for(String simNation : nations) {
 				if(simNation.equals(nation)) {
